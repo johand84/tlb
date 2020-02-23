5b63f757eaead323c63dddf141b713e767fc7a49
Johan Andersson <johan.andersson.1984@bredband.net>
2020-02-23 23:38:05 +0100

    Added the function fetch_decode to Source_to_MachineProofs.thy

--- TLB_COMP/Source_to_MachineProofs.thy
+++ TLB_COMP/Source_to_MachineProofs.thy
@@ -3,4 +3,10 @@ theory Source_to_MachineProofs
 imports Source_to_Machine
 
 begin
+
+fun
+  fetch_decode :: "'a::mmu state_scheme \<Rightarrow> instruction \<times> 'a::mmu state_scheme"
+where
+  "fetch_decode s = (let (m,s') = Fetch s in Decode m s')"
+
 end
