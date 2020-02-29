0c489aa05c964d794dba3ddfab323cde19dcbc7c
Johan Andersson <johan.andersson.1984@bredband.net>
2020-02-29 18:09:19 +0100

    Added the function comp_aunop in Source_to_Machine.thy

--- TLB_COMP/Source_to_Machine.thy
+++ TLB_COMP/Source_to_Machine.thy
@@ -1,8 +1,15 @@
 theory Source_to_Machine
 
-imports TLB_COMP.Logic
+imports ARM_ConditionalMnemonic
+        TLB_COMP.Logic
         TLB_COMP.MMU_Instants_TLB_PDC
 
 
 begin
+
+fun
+  comp_aunop :: "aunop \<Rightarrow> instruction list"
+where
+  "comp_aunop Neg = [neg 0 0]"
+
 end
