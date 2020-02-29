49d30461dd167dc97ec9c5bac0ca51aaf04a0f26
Johan Andersson <johan.andersson.1984@bredband.net>
2020-02-29 19:44:46 +0100

    Added the function comp_abinop in Source_to_Machine.thy

--- TLB_COMP/Source_to_Machine.thy
+++ TLB_COMP/Source_to_Machine.thy
@@ -12,4 +12,10 @@ fun
 where
   "comp_aunop Neg = [neg 0 0]"
 
+fun
+  comp_abinop :: "abinop \<Rightarrow> instruction list"
+where
+  "comp_abinop Plus = [add_reg 0 0 1]" |
+  "comp_abinop Minus = [sub_reg 0 0 1]"
+
 end
