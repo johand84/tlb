e836ba539c7c3bcde7d920ca46911047412ae439
Johan Andersson <johan.andersson.1984@bredband.net>
2020-03-05 12:33:27 +0100

    Added the function register_related to ARM_MnemonicProofs.thy

--- TLB_COMP/ARM_MnemonicProofs.thy
+++ TLB_COMP/ARM_MnemonicProofs.thy
@@ -10,6 +10,17 @@ where
     \<forall>x. m x = m' x
   )"
 
+fun
+  register_related :: "(RName \<Rightarrow> 32 word) \<Rightarrow> (RName \<Rightarrow> 32 word) \<Rightarrow> (RName \<rightharpoonup> 32 word) \<Rightarrow> RName \<Rightarrow> bool"
+where
+  "register_related rs rs' rf r = (
+    let y = rs' r
+    in (
+      case rf r of Some x \<Rightarrow> x = y |
+                   None \<Rightarrow> rs r = y
+    )
+  )"
+
 lemma "\<lbrakk>
     Encoding s = Encoding_ARM;
     Extensions s = {}
