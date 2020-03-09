a5831879c0ed5c9c71ece83dd8d544170ff55df8
Johan Andersson <johan.andersson.1984@bredband.net>
2020-03-09 22:37:13 +0100

    Added the definition reg_to_bin to ARM_MnemonicProof.thy

--- TLB_COMP/ARM_MnemonicProofs.thy
+++ TLB_COMP/ARM_MnemonicProofs.thy
@@ -59,10 +59,33 @@ where
     arm_registers_related (REG s) (REG s') rf
   )"
 
+definition
+  reg_to_bin :: "RName \<Rightarrow> 4 word"
+where
+  "reg_to_bin r = (
+    if r = RName_0usr then 0
+    else if r = RName_1usr then 1
+    else if r = RName_2usr then 2
+    else if r = RName_3usr then 3
+    else if r = RName_4usr then 4
+    else if r = RName_5usr then 5
+    else if r = RName_6usr then 6
+    else if r = RName_7usr then 7
+    else if r = RName_8usr then 8
+    else if r = RName_9usr then 9
+    else if r = RName_10usr then 10
+    else if r = RName_11usr then 11
+    else if r = RName_12usr then 12
+    else if r = RName_SPusr then 13
+    else if r = RName_LRusr then 14
+    else if r = RName_PC then 15
+    else HOL.undefined
+  )"
+
 lemma add_reg_proof: "\<lbrakk>
     Encoding s = Encoding_ARM;
     Extensions s = {};
-    snd (Run (add_reg 0 0 1) s) = s';
+    snd (Run (add_reg (reg_to_bin RName_0usr) (reg_to_bin RName_0usr) (reg_to_bin RName_1usr)) s) = s';
     ((REG s) RName_0usr) = 0;
     ((REG s) RName_1usr) = 1
   \<rbrakk> \<Longrightarrow> arm_state_related s s' (\<lambda>x. (if x = RName_0usr then Some 1 else None))
@@ -92,6 +115,7 @@ lemma add_reg_proof: "\<lbrakk>
         dfn'Register_def
         doRegister_def
         mask_def
+        reg_to_bin_def
         snd_def
         word_bits_def
         word_extract_def
@@ -105,7 +129,7 @@ lemma add_reg_proof: "\<lbrakk>
 lemma mov_imm_proof: "\<lbrakk>
     Encoding s = Encoding_ARM;
     Extensions s = {};
-    snd (Run (mov_imm 0 0) s) = s'
+    snd (Run (mov_imm (reg_to_bin RName_0usr) 0) s) = s'
   \<rbrakk> \<Longrightarrow> arm_state_related s s' (\<lambda>x. (if x = RName_0usr then Some 0 else None))
 "
   apply (
@@ -131,6 +155,7 @@ lemma mov_imm_proof: "\<lbrakk>
         dfn'ArithLogicImmediate_def
         mask_def
         mov_imm_def
+        reg_to_bin_def
         snd_def
         ucast_def
         word_extract_def
