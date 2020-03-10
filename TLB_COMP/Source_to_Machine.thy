d9c5d61b30a9d4679420bebe2213fe234a9ce2f6
Johan Andersson <johan.andersson.1984@bredband.net>
2020-03-10 01:10:34 +0100

    Updated Source_to_Machine.thy

--- TLB_COMP/Source_to_Machine.thy
+++ TLB_COMP/Source_to_Machine.thy
@@ -54,12 +54,11 @@ where
   "comp_bexp (BBinOp op b1 b2) = comp_bexp b2 @ push 0 # comp_bexp b1 @ pop 1 # comp_bbinop op" |
   "comp_bexp (BComp op a1 a2) = comp_aexp a2 @ push 0 # comp_aexp a1 @ pop 1 # comp_bcomp op"
 
-fun code_size ::
-  "instruction list \<Rightarrow> 32 word"
+fun
+  code_size :: "instruction list \<Rightarrow> 32 word"
 where
   "code_size [] = 0" |
-  "code_size ((IfThen _) # is) = code_size is" |
-  "code_size (i#is) = code_size is + 4"
+  "code_size (i#is) = code_size is + 1"
 
 fun
   comp_com :: "com \<Rightarrow> instruction list"
@@ -70,11 +69,25 @@ where
   "comp_com (If b c1 c2) = (
     let i1 = comp_com c1;
         i2 = comp_com c2
-    in comp_bexp b @ cmp_imm 0 0 # beq_imm ((code_size i1) + 4) @ i1 @ b_imm (code_size i2) # i2
+    in (
+      comp_bexp b @
+      cmp_imm 0 0 #
+      beq_imm ((code_size i1)-1) @
+      i1 @ b_imm ((code_size i2)-2) #
+      i2
+    )
   )" |
+(* *)
   "comp_com (While b c) = (
-    let i = comp_com c
-    in comp_bexp b @ cmp_imm 0 0 # beq_imm ((code_size i) + 4) @ i @ [b_imm (code_size i)]
+    let i1 = comp_bexp b;
+        i2 = comp_com c
+    in (
+      i1 @
+      cmp_imm 0 0 #
+      beq_imm ((code_size i2)-1) @
+      i2 @
+      [b_imm (-((code_size i1) + (code_size i2) + 4))]
+    )
   )" |
   "comp_com c = []"
 
