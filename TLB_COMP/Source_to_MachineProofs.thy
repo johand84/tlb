ed1ce295376fe9f80f9c5f82642f266795eb19c2
Johan Andersson <johan.andersson.1984@bredband.net>
2020-03-16 11:33:28 +0100

    Removed the function fetch_decode from Source_to_MachineProofs.thy

--- TLB_COMP/Source_to_MachineProofs.thy
+++ TLB_COMP/Source_to_MachineProofs.thy
@@ -5,11 +5,6 @@ imports Source_to_Machine
 begin
 
 fun
-  fetch_decode :: "'a::mmu state_scheme \<Rightarrow> instruction \<times> 'a::mmu state_scheme"
-where
-  "fetch_decode s = (let (m,s') = Fetch s in Decode m s')"
-
-fun
   code_installed :: "'a::mmu state_scheme \<Rightarrow> instruction list \<Rightarrow>  bool"
 where
   "code_installed s [] = True" |
