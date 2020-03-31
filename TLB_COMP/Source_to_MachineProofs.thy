d9100da3d211b1a6cb27763231a8158343a22402
Johan Andersson <johan.andersson.1984@bredband.net>
2020-03-31 17:48:51 +0200

    Added the function code_installed to Source_to_MachineProofs.thy

--- TLB_COMP/Source_to_MachineProofs.thy
+++ TLB_COMP/Source_to_MachineProofs.thy
@@ -3,4 +3,16 @@ theory Source_to_MachineProofs
 imports Source_to_Machine
 
 begin
+
+fun
+  code_installed :: "'a set_tlb_state_scheme \<Rightarrow> MachineCode list \<Rightarrow>  bool"
+where
+  "code_installed s [] = True" |
+  "code_installed s (x#xs) = (
+    case Fetch s of (m,t) \<Rightarrow> m = x \<and> code_installed (
+        t\<lparr>REG := (REG s)(RName_PC := REG s RName_PC + 4)\<rparr>
+      )
+      xs
+  )"
+
 end
