70326d1725ae4d7a80f309774b5f3b04485c8e39
Johan Andersson <johan.andersson.1984@bredband.net>
2020-04-17 11:26:32 +0200

    Added the lemma comp_aexp_BinOp_correct to Source_to_MachineProofs.thy

--- TLB_COMP/Source_to_MachineProofs.thy
+++ TLB_COMP/Source_to_MachineProofs.thy
@@ -349,4 +349,16 @@ lemma comp_aexp_BinOp_Minus_correct:
   apply (simp add: steps_add steps_inc)
   done
 
+lemma comp_aexp_BinOp_correct:
+  "\<lbrakk>\<lbrakk>e\<rbrakk> s = Some val; code_installed t (comp_aexp e @ ins); state_rel s t; e = BinOp op val1 val2\<rbrakk> \<Longrightarrow>
+    \<exists>t'. steps t (length (comp_aexp e)) = t' \<and>
+      code_installed t' ins \<and>
+      state_rel s t' \<and>
+      state.REG t' RName_0usr = val \<and>
+      REG t' RName_2usr = REG t RName_2usr"
+  apply (cases op)
+   apply (rule comp_aexp_BinOp_Plus_correct, force+)
+  apply (rule comp_aexp_BinOp_Minus_correct, force+)
+  done
+
 end
