f3016f69661a09c6f7b55a01b5f1acf1134f8bc7
Johan Andersson <johan.andersson.1984@bredband.net>
2020-04-22 19:55:04 +0200

    Added the lemma Decode_and_reg_correct to Source_to_MachineProofs.thy

--- TLB_COMP/Source_to_MachineProofs.thy
+++ TLB_COMP/Source_to_MachineProofs.thy
@@ -81,4 +81,8 @@ lemma Decode_add_reg_correct:
   "Decode (add_reg rd rn rm) t = (i,t') \<Longrightarrow> i = Data (Register (0x4, False, rd, rn, rm, SRType_LSL, 0))"
   sorry
 
+lemma Decode_and_reg_correct:
+  "Decode (and_reg rd rn rm) t = (i,t') \<Longrightarrow> i = Data (Register (0x0, False, rd, rn, rm, SRType_LSL, 0))"
+  sorry
+
 end
