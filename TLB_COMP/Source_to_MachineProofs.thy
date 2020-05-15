9000a7b9d3856e0bd6dd4fc4e3ed5af2afa97bb6
Johan Andersson <johan.andersson.1984@bredband.net>
2020-05-15 16:28:20 +0200

    Updated the lemma Run_add_reg_correct in Source_to_MachineProofs.thy

--- TLB_COMP/Source_to_MachineProofs.thy
+++ TLB_COMP/Source_to_MachineProofs.thy
@@ -491,7 +491,7 @@ lemma Run_add_reg_correct:
     general_purpose_reg rn;
     general_purpose_reg rm\<rbrakk> \<Longrightarrow>
       machine_config t \<and>
-      machine_config_preserved s t \<and>
+      machine_state_preserved s t \<and>
       REG t = (REG s)(bin_to_reg rd := REG s (bin_to_reg rn) + REG s (bin_to_reg rm),
                       RName_PC := REG s RName_PC + 4)"
   apply (simp add: Run_def dfn'Register_def doRegister_def split: prod.splits)
@@ -509,7 +509,7 @@ lemma Run_add_reg_correct:
   apply (frule write'R_correct, simp, simp, simp, safe)
     apply (frule IncPC_correct, simp, safe)
    apply (frule IncPC_correct, simp, safe)
-   apply (simp add: machine_config_def machine_config_preserved_def)
+   apply (simp add: machine_config_def machine_state_preserved_def)
   apply (simp add: AddWithCarry_def Let_def wi_hom_syms, safe)
   apply (frule IncPC_correct, simp, simp)
   apply (frule general_purpose_reg_correct, safe)
