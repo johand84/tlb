7ef227fce8f9e0aa0c41049fc49203e2b639d2f6
Johan Andersson <johan.andersson.1984@bredband.net>
2020-05-15 22:21:29 +0200

    Updated the lemma comp_bexp_mov_correct in Source_to_MachineProofs.thy

--- TLB_COMP/Source_to_MachineProofs.thy
+++ TLB_COMP/Source_to_MachineProofs.thy
@@ -1310,6 +1310,7 @@ lemma comp_bexp_mov_correct:
         REG t' = (REG t)(bin_to_reg reg := (if val then 1 else 0),
                          RName_PC := REG t RName_PC + 4 * (word_of_int (int (length (comp_bexp_mov reg val)))))"
   apply (simp add: comp_bexp_mov_def split: prod.splits)
+  apply (frule code_installed_implies_Fetch)
   apply (frule mov_imm_correct, simp, simp)
    apply (simp add: word_bits_def word_extract_def, safe)
        apply (simp add: state_rel_preserved)+
